4b2c49c0bcade7ca2abd29933770281460d2335c
abumahid <eng.marufbilla@gmail.com>
2025-10-26 12:51:49 +0600

    update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello mauyar pooo\n');
+  res.end('Hello mauyar poooooo\n');
 });
 
 server.listen(port, () => {
