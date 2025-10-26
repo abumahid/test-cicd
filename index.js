4315c7391228c2e38a32aec17273c98ca0a04003
abumahid <eng.marufbilla@gmail.com>
2025-10-26 12:48:27 +0600

    update

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello mauyar po\n');
+  res.end('Hello mauyar pooo\n');
 });
 
 server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
+
+
